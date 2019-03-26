2ce9b7c3035ee923060b92f09e8a245f8654b950
Yun <yunfah.chow@gmail.com>
2019-03-26 11:11:49 +0100

    Keybindings are now in rules

--- src/ui/HelpUI.java
+++ src/ui/HelpUI.java
@@ -38,13 +38,19 @@ public class HelpUI extends JPanel implements ActionListener{
 			+ "\n" + "The weaker piece is removed from the board, and the stronger piece is moved into the place formerly occupied by the weaker piece. "
 			+ "\n" + "If the engaging pieces are of equal rank, they are both removed. "
 			+ "\n" + "Pieces may not move onto a square already occupied by another piece without attacking."
-			+ "\n" + "However, when the marshal attacks the spy, the spy loses. Bombs lose when they are defused by a miner."
+			+ "\n" + "However, when the marshal(level 10) attacks the spy(level 1), the spy loses. Bombs lose when they are defused by a miner(level 3)."
 			+ "\n" + "The bombs and the flag cannot be moved. A bomb defeats every piece that tries to attack it, except the miner."
 			+ "\n" 
 			+ "\n" + "The flag loses from every other piece. When you capture the flag of your opponent you win the game."
 			+ "\n" 
 			+ "\n" + "The Stratego board consists of 10 x 10 squares." 
 			+ "\n" + "Within the board there are two obstacles of 2 x 2 squares each. Pieces are not allowed to move there."
+			+ "\n" 
+			+ "\n" + " " 
+			+ "\n" + "Keybindings"
+			+ "\n" 
+			+ "\n" + "Rightclick removes a placed piece during the placement phase. It places it back to the list of unplaced pieces." 
+			+ "\n" + "Leftclick selects. It is the main button used to navigate in the application." 
 
 ;
 
